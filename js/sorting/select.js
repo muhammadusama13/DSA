311c6428abd96eddeacffc7d17a31376d7952349
Abdul <muhammadusama1390@gmail.com>
2024-10-21 20:24:44 +0500

    FEAT:sorting algorithms

---
+++ js/sorting/select.js
@@ -0,0 +1,22 @@
+const array = [12, 5, 4, 10, 3, 2, 16]
+
+for (let i = 0; i < array.length; i++) {
+    let min = i
+    for (let j = i; j < array.length; j++) {
+
+        if (array[min] > array[j]) {
+            min = j
+        }
+    }
+    // console.log('ST', array[i], 'EN', array[min])
+    let temp = array[i]
+    array[i] = array[min]
+    // console.log('ST2', array[i], 'EN2', array[min])
+    array[min] = temp
+    // console.log('ST3', array[i], 'EN3', array[min])
+
+    // console.log('array', array)
+}
+
+
+console.log(array)
